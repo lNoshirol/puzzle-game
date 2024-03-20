c7d2953bcd1d2ae9646c8f8632f09415552de619
Lucas Pécout <lucas.pecout@e-artsup.net>
2024-03-20 11:47:10 +0100

    cable plus ou moins good

--- puzzle jam/Assets/script/LineConnector.cs
+++ puzzle jam/Assets/script/LineConnector.cs
@@ -11,11 +11,12 @@ public class LineConnector : MonoBehaviour
     private void Start()
     {
         line = this.gameObject.GetComponent<LineRenderer>();
+        line.positionCount = Objs.Length;
     }
 
     private void Update()
     {
-        for (int i = 1-0; i < Objs.Length; i++)
+        for (int i = 0; i < Objs.Length; i++)
         {
             line.SetPosition(i, Objs[i].transform.position);
         }
