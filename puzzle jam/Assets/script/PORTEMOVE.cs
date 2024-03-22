5348c8fb02efbebc9ffb0d5cca164006aa0201dd
Lucas Pécout <lucas.pecout@e-artsup.net>
2024-03-22 12:24:17 +0100

    ALED

---
+++ puzzle jam/Assets/script/PORTEMOVE.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PORTEMOVE : MonoBehaviour
+{
+    public int MONTEOUDESCEND;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < 0)
+        {
+            MONTEOUDESCEND = 0;
+            transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+        }
+        else if (transform.position.y > 5)
+        {
+            MONTEOUDESCEND = 0;
+            transform.position = new Vector3(transform.position.x, 5, transform.position.z);
+        }
+        else if (transform.position.y >= 0 && transform.position.y <= 5)
+        {
+            transform.position += new Vector3(0, MONTEOUDESCEND, 0) * Time.deltaTime * 2;
+        }
+    }
+}
